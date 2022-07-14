6c6ec1ebaffdcf1baa6d3b7323c4c2a2f614c9fa
yoyochu <yolanda.chu@yahoo.com.au>
2022-07-14 15:08:35 +0800

    fix

---
+++ mkdirweek2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+read -p "type the name of the folder you would like to create" folderName
+
+mkdir "$folderName"
+					
---
+++ portfolio/week_2/setPassword.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#this command is asking which folder would the user want to navigate to. 
+read -p "Type the name of the folder you would like to create: " FolderName
+#this command makes the new folder
+mkdir $FolderName
+#navigate to the new folder
+cd /home/yochu/scripts/portfolio/week_2/$FolderName
+#this command is asking for the password from user
+read -sp "Enter Password:" Password
+#storing the password into secret.txt
+#echo $Password > secret.txt
+#storing the password into secret.txt in a hash
+echo $Password | sha256sum > secret.txt
+exit 20
