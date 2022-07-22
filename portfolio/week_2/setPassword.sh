6af4d7ff40d16a8618ec6b9465d6620bc5714765
yoyochu <yolanda.chu@yahoo.com.au>
2022-07-22 23:30:13 +0800

    updated the set password script to encrypt to file

--- portfolio/week_2/setPassword.sh
+++ portfolio/week_2/setPassword.sh
@@ -4,11 +4,12 @@ read -p "Type the name of the folder you would like to create: " FolderName
 #this command makes the new folder
 #mkdir $FolderName
 #navigate to the new folder
-cd /home/yochu/scripts/portfolio/week_2/$FolderName
+#cd /home/yochu/scripts/portfolio/week_2/$FolderName
 #this command is asking for the password from user
 read -sp "Enter Password:" Password
 #storing the password into secret.txt
-echo $Password > secret.txt
+#echo $Password > secret.txt
+echo $Password | openssl enc -base64 -e -aes-256-cbc -nosalt  -pass pass:garbageKey  > secret.lck
 #storing the password into secret.txt in a hash
 #echo $Password | sha256sum > secret.txt
 echo "Password Changed"
