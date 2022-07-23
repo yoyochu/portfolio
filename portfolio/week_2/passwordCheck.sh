8a85c0f566acdf5410a21beecd8063b90b933e4c
yoyochu <yolanda.chu@yahoo.com.au>
2022-07-23 19:44:39 +0800

    update code to add colour

--- portfolio/week_2/passwordCheck.sh
+++ portfolio/week_2/passwordCheck.sh
@@ -1,22 +1,22 @@
 #! /bin/bash
+#Variables for colours
+RED='\033[31m'
+NORMAL='\033[00m'
+GREEN='\033[32m'
 
-#this command is asking for the password from user
-read -sp "Enter Password:" Password
-echo
-#echo "Password entered: $Password"
-#echo "Secret.txt contents: $(cat secret.txt)"
-##Check the user’s password against the hash stored in secret.lck
-#echo "Secret.txt contents: $(cat secret.lck)"
+#user to enter password
+echo -e "${RED}Enter Password:${NORMAL} "
+read -s Password
 #decrypting the .lck file 
-PASS=`cat secret.lck | openssl enc -base64 -d -aes-256-cbc -nosalt -pass pass:garbageKey
+PASS=`cat /home/yochu/scripts/portfolio/week_2/secret.lck | openssl enc -base64 -d -aes-256-cbc -nosalt -pass pass:garbageKey
 `
 #comparing the password entered against the decrypted password.
 if [ "$Password" == "$PASS" ]; then 
 #If the user’s password is correct, print “Access Granted” and quit with the exit code 0.
-echo "Access Granted"
+echo "${GREEN} Access Granted ${NORMAL}"
 exit 0 
 else
 #If the user’s password is incorre1ct print “Access Denied” and quit with the exit code 1.
-echo "Access Denied"
+echo -e "${GREEN} Access Denied ${NORMAL}"
 exit 1
 fi
