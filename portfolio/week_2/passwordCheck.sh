9943c22a24ed73af8038d324b5dfd5201b3a02b9
yoyochu <yolanda.chu@yahoo.com.au>
2022-07-24 16:50:58 +0800

    updated the code to make sure echo was displaying green

--- portfolio/week_2/passwordCheck.sh
+++ portfolio/week_2/passwordCheck.sh
@@ -13,7 +13,7 @@ PASS=`cat /home/yochu/scripts/portfolio/week_2/secret.lck | openssl enc -base64 
 #comparing the password entered against the decrypted password.
 if [ "$Password" == "$PASS" ]; then 
 #If the user’s password is correct, print “Access Granted” and quit with the exit code 0.
-echo "${GREEN} Access Granted ${NORMAL}"
+echo -e "${GREEN} Access Granted ${NORMAL}"
 exit 0 
 else
 #If the user’s password is incorre1ct print “Access Denied” and quit with the exit code 1.
