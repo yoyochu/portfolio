9912bdf05e6bd06a447bc6db6ddf76cf3cef25cc
yoyochu <yolanda.chu@yahoo.com.au>
2022-07-28 22:13:25 +0800

    Updated the set password script to use sha256sum

--- portfolio/week_2/setPassword.sh
+++ portfolio/week_2/setPassword.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 #this command is asking which folder would the user want to navigate to. 
-read -p "Type the name of the folder you would like to create: " FolderName
+#read -p "Type the name of the folder you would like to create: " FolderName
 #this command makes the new folder
 #mkdir $FolderName
 #navigate to the new folder
 #cd /home/yochu/scripts/portfolio/week_2/$FolderName
 #this command is asking for the password from user
 read -sp "Enter Password:" Password
-#storing the password into secret.txt
-#echo $Password > secret.txt
-echo $Password | openssl enc -base64 -e -aes-256-cbc -nosalt  -pass pass:garbageKey  > secret.lck
-#storing the password into secret.txt in a hash
-#echo $Password | sha256sum > secret.txt
+#storing the password into secret.txt with hash
+echo $Password | sha256sum > secret.txt
+echo
 echo "Password Changed"
 exit 20
