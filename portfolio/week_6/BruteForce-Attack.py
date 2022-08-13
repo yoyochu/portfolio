63e61358db9e00cd88b33f6fcd0e79fcddc48dcd
yoyochu <yolanda.chu@yahoo.com.au>
2022-08-13 21:22:22 +0800

    week6 scripts

---
+++ portfolio/week_6/BruteForce-Attack.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import hashlib
+import itertools
+import string
+
+#hidden password hash
+password = "zBlah"
+passwordHash = hashlib.sha256(password.encode("utf-8")).hexdigest()
+
+#Cracking the code with for loop --
+
+flagg=0
+chars=string.ascii_letters + string.digits
+for password_length in range(1, 8):
+    for guess in itertools.product(chars, repeat=password_length):
+        guess = ''.join(guess)
+        digest = hashlib.sha256(guess.encode("utf-8")).hexdigest()
+        
+        if digest == passwordHash:
+            print("password is: " + guess)
+            flagg=1
+            
+            break;
+    if flagg==1:
+        break;
+        
+if flagg==0:
+    print("password is over 8 characters long")
