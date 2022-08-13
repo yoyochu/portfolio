db24aa39b36d6631b6e6205beb732a1c83cb07dc
yoyochu <108972799+yoyochu@users.noreply.github.com>
2022-08-13 14:24:05 +0800

    Delete scripts directory

--- scripts/portfolio/week_1/second.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Hi there!"
-
-echo "It's good to see you $1!"
-
-exit 0
-				
--- scripts/portfolio/week_2/setPassword.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Enter Password:"
-read -s Password
-#storing the password into secret.txt with hash
-echo -n $Password | sha256sum > /home/yochu/scripts/portfolio/week_2/secret.txt
-echo
-echo "Password Changed"
-exit 0
